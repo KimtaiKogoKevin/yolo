9e98eb07b014d4cb405259ca93201072c0701170
KimtaiKogoKevin <kevinkogo@gmail.com>
2023-04-20 17:28:48 +0300

    added this commit

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url =  "mongodb://kevinkogo:4DKRzGoYbWwS3iku@10.40.0.91:27017"
+let mongodb_url =  "mongodb://kevinkogo:4DKRzGoYbWwS3iku@10.44.0.86:27017"
 
 // let mongodb_url = "mongodb://kevinkogo:4DKRzGoYbWwS3iku@mongodb:27017/"
 // let mongodb_url='mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@mongodb:27017/'
